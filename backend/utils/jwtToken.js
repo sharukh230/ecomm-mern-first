acd85fc7b9fcd25dc85128c4a5029e31bd569800
Sharukh Dastageer Shaikh <107460351+sharukh230@users.noreply.github.com>
2023-01-14 15:58:44 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,6 +8,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+     httpOnly: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
